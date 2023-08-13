36f32a6691835269daa09388f69c443f42b7f426
MateoNerli <mnerli2003@gmail.com>
2023-08-13 19:35:19 -0300

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,63 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Navbar } from './componentes/nav/nav.component';
+import { Header } from './componentes/header/header.component';
+import { SobreMi } from './componentes/sobreMi/sobreMi.component';
+import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { HardSoftSkillsComponent } from './componentes/hard-soft-skills/hard-soft-skills.component';
+import { ProyectoComponent } from './componentes/proyecto/proyecto.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './componentes/home/home.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { NewExperienciaComponent } from './componentes/experiencia/new-experiencia.component';
+import { interceptorProvider } from './service/interceptor-service';
+import { ReactiveFormsModule} from '@angular/forms';
+import { EditExperienciaComponent } from './componentes/experiencia/edit-experiencia.component';
+import { NeweducacionComponent } from './componentes/educacion/neweducacion.component';
+import { EditeducacionComponent } from './componentes/educacion/editeducacion.component';
+import { RouterModule } from '@angular/router';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    Header,
+    Navbar,
+    SobreMi,
+    ExperienciaComponent,
+    EducacionComponent,
+    HardSoftSkillsComponent,
+    ProyectoComponent,
+    FooterComponent,
+    HomeComponent,
+    LoginComponent,
+    NewExperienciaComponent,
+    EditExperienciaComponent,
+    NeweducacionComponent,
+    EditeducacionComponent,
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule,
+    FormsModule,
+    AppRoutingModule,
+    FormsModule,
+    NgCircleProgressModule.forRoot({}),
+    HttpClientModule,
+    ReactiveFormsModule
+  ],
+  providers: [
+    interceptorProvider
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
---
+++ src/app/componentes/sobreMi/sobreMi.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { persona } from 'src/app/modelo/persona.modelo';
+import { PersonaService } from 'src/app/service/persona.service';
+
+@Component({
+  selector: 'SobreMi',
+  templateUrl: './sobreMi.component.html',
+  styleUrls: ['./sobreMi.component.css']
+})
+
+export class SobreMi implements OnInit {
+
+  persona: persona = new persona("","","");
+
+  constructor(public personaService: PersonaService) { }
+  
+  ngOnInit(): void {
+    this.personaService.getPersona().subscribe(data => { this.persona = data })
+  }
+
+}
